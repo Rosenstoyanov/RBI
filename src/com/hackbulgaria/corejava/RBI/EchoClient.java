57387e1333d22f5d0ef70bceb47a22627270bce9
adria-stef <adria.stef@gmail.com>
2014-07-17 13:50:24 +0300

    added EchoClient

--- src/com/hackbulgaria/corejava/RBI/EchoClient.java
+++ src/com/hackbulgaria/corejava/RBI/EchoClient.java
@@ -1,5 +1,35 @@
 package com.hackbulgaria.corejava.RBI;
 
-public class EchoClient {
+//package com.hackbulgaria.corejava.echoclient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class EchoClient {
+    public static void main(String[] args) throws IOException {
+
+        String hostName = "192.168.1.45";
+        int portNumber = 1436;
+
+        try (Socket echoSocket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
+            System.exit(1);
+        }
+    }
 }
